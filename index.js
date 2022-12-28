1dda6a8dd27bce80cd2adf2dfdb67e6cef3064a8
Alexis <alexisburgos1997@gmail.com>
2022-12-27 21:43:43 -0300

    CORS y Express - primer commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
+
+//Crear servidor express
+const app = express();
+
+//Configurar CORS
+app.use( cors() );
+
+//Base de datos
+dbConnection();
+
+//Rutas
+app.get("/", (req, resp) => {
+    resp.status(200).json({
+        ok: true,
+        msg: "Hola mundo"
+    });
+});
+
+app.listen( process.env.PORT, () => {
+    console.log( "Servidor corriendo en el puerto " + process.env.PORT );
+});
